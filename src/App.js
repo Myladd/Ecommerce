3b666d8c9b2c0a1b40d96b8ac790156564ba1c6d
myladd <mylad.zandkarimi@gmail.com>
2022-08-04 23:46:33 +0430

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import MenuOne from './components/Menu';
+// import MenuOne from './components/Menu';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+
 
 function App() {
   return (
-      <MenuOne/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Admin' element={<Admin/>} />
+      </Routes>
+    </Router>
   );
 }
 
