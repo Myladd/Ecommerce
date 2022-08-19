f7d5a0e4e59a52d396cc76364fc84a245eea97bc
myladd <mylad.zandkarimi@gmail.com>
2022-08-19 13:58:20 +0430

    create popover count and price table

--- src/components/Pagination/CountAndPrice.jsx
+++ src/components/Pagination/CountAndPrice.jsx
@@ -1,52 +1,83 @@
-import { Flex, Image, Spinner, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import React from 'react'
-import melon from "assets/images/melon.png"
+import { Button, Flex, Image, Input, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Spinner, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import React from "react";
+import melon from "assets/images/melon.png";
 
-function CountAndPrice({posts, loading}) {
+function CountAndPrice({ posts, loading }) {
   if (loading) {
-    return <Spinner
-      style={{
-        "display": "flex",
-        "justifyContent": "center",
-        "margin": "auto"
-      }}
-    thickness='4px'
-    speed='0.65s'
-    emptyColor='gray.200'
-    color='blue.500'
-    size='xl'
-    />;
-    
+    return (
+      <Spinner
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "auto",
+        }}
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    );
   }
+
+  const handleEdit = () => {
+    return <Input placeholder="Basic usage" />;
+  };
 
   return (
     <>
-    <Flex justify="center" w>
-      <TableContainer maxW="1200px">
-        <Table w="800px" variant="striped" colorScheme="teal">
-          <Thead>
-            <Tr>
-              <Th>تصویر کالا</Th>
-              <Th>نام کالا</Th>
-              <Th>تعداد</Th>
-              <Th>قیمت</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
+      <Flex justify="center" w>
+        <TableContainer maxW="1200px">
+          <Table w="800px" variant="striped" colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>تصویر کالا</Th>
+                <Th>نام کالا</Th>
+                <Th>تعداد</Th>
+                <Th>قیمت</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
               {posts.map((post) => (
                 <Tr key={post.id}>
-                  <Td><Image borderRadius="full" boxSize="40px" src={melon} alt="melon" /></Td>
+                  <Td>
+                    <Image borderRadius="full" boxSize="40px" src={melon} alt="melon" />
+                  </Td>
                   <Td>{post.name}</Td>
-                  <Td>{post.count}</Td>
-                  <Td>{post.price}</Td>
+                  <Td>
+                    <Popover>
+                      <PopoverTrigger>
+                      <Button>{post.count}</Button>
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        <PopoverArrow />
+                        {/* <PopoverCloseButton dir="ltr"/> */}
+                        <PopoverHeader>تعداد را وارد کنید</PopoverHeader>
+                        <PopoverBody><Input placeholder='تعداد به عدد' /></PopoverBody>
+                      </PopoverContent>
+                    </Popover>
+                  </Td>
+                  <Td>
+                  <Popover>
+                      <PopoverTrigger>
+                      <Button>{post.price}</Button>
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        <PopoverArrow />
+                        {/* <PopoverCloseButton dir="ltr"/> */}
+                        <PopoverHeader>قیمت را وارد کنید</PopoverHeader>
+                        <PopoverBody><Input placeholder='قیمت به عدد' /></PopoverBody>
+                      </PopoverContent>
+                    </Popover>
+                  </Td>
                 </Tr>
               ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Flex>
-  </>
-  )
+    </>
+  );
 }
 
-export default CountAndPrice
+export default CountAndPrice;
